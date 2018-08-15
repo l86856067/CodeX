e58f0c864ac3a098a769800db19f2b5e0604ca78
liuhui <782247668@qq.com>
2018-08-15 18:44:56 +0800

    添加了 三个包名，以及main 添加了新的按钮

--- app/src/main/java/com/example/liuhui/codex/MainActivity.java
+++ app/src/main/java/com/example/liuhui/codex/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.liuhui.codex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button main_btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        setListener();
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.main_btn1:
+                break;
+        }
+    }
+
+    private void setListener() {
+        main_btn1.setOnClickListener(this);
+    }
+
+    private void initView() {
+        main_btn1 = (Button) findViewById(R.id.main_btn1);
+    }
+
 }
